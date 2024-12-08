52495dc67298adb6a35aaf90ef976983ed907672
TheTuanub244 <tuan.mt211010@sis.hust.edu.vn>
2024-12-08 16:08:55 +0700

    add

--- index.js
+++ index.js
@@ -7,10 +7,16 @@ require("dotenv").config();
 
 app.use(express.json());
 
-connectDB()
+async function setupMqttAndMongo() {
+    try {
+        
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+    }
+}
+
 
 module.exports = async (req, res) => {
-    connectDB();
 
     console.log("HTTP request received");
     res.status(200).send("MQTT and MongoDB setup are running successfully.");
