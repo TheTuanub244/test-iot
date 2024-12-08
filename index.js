11a4a8e2d164c8fb02d7d6d23bbfc6517c34a627
TheTuanub244 <tuan.mt211010@sis.hust.edu.vn>
2024-12-08 16:41:17 +0700

    add

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ require("dotenv").config();
 
 app.use(express.json());
 const mongoUri = process.env.MONGO_URI || "mongodb+srv://tuanub244:AtOePUkyLEbKvv16@it4409.0ybva.mongodb.net/?retryWrites=true&w=majority&appName=it4409"
-const client = new MongoClient(mongoUri, {
-    serverSelectionTimeoutMS: 50000 
-});
+
 
 async function setupMqttAndMongo() {
     try {
