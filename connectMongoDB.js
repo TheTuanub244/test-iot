5c41a89decb1a7d24c9803e25bf1efb1be6e4876
TheTuanub244 <tuan.mt211010@sis.hust.edu.vn>
2024-12-08 16:28:51 +0700

    add

--- connectMongoDB.js
+++ connectMongoDB.js
@@ -5,7 +5,9 @@ const connectDB = async () => {
         await mongoose.connect(process.env.MONGODB_URI)
         .then(() => {
             console.log("Connected to MongoDB")
-            const database = mongoose.connections;
+            const database1 = mongoose.connections.model;
+            const database2 = mongoose.connections.Schema;
+
             console.log(database)
         })
     } catch (error) {
