f2d7e783252bf510f91017dd7d1149106a598728
TheTuanub244 <tuan.mt211010@sis.hust.edu.vn>
2024-12-08 18:04:05 +0700

    update

--- connectMongoDB.js
+++ connectMongoDB.js
@@ -4,12 +4,9 @@ const connectDB = async () => {
         await mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => {
             console.log("Connected to MongoDB")
-            const database1 = mongoose.connections.model;
-            const database2 = mongoose.connections.Schema;
-
-            console.log(database1)
-            console.log(database2)
-            console.log(mongoose.connection.db)
+            const database = mongoose.connection.db
+            database.collection('bookings').find().toArray()
+                .then(test => console.log(test))
         })
 }
 
