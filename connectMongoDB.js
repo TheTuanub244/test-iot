565b7ca62a13f6788291ff0be1c357aa6f7c7c3f
TheTuanub244 <tuan.mt211010@sis.hust.edu.vn>
2024-12-08 16:34:58 +0700

    add

--- connectMongoDB.js
+++ connectMongoDB.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const { MongoClient } = require('mongodb');
+const mongoUri = process.env.MONGO_URI || "mongodb+srv://vercel-admin-user:fUMbRXDPALiyi43s@cluster0.jin5c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 const connectDB = async () => {
     const client = new MongoClient(mongoUri, {
